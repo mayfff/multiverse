fb38439a91bbbb50450460835b4072a13f3997e6
Mayfff <zakrevsky.d.s@gmail.com>
2024-05-30 22:27:06 +0300

    fixed some flaws

--- src/main/java/coursework/multiverse/security/CustomUserDetailsService.java
+++ src/main/java/coursework/multiverse/security/CustomUserDetailsService.java
@@ -29,7 +29,7 @@ public class CustomUserDetailsService implements UserDetailsService {
                     user.getPassword(),
                     mapRolesToAuthorities(user.getRoles()));
         } else {
-            throw new UsernameNotFoundException("Invalid username or password.");
+            throw new UsernameNotFoundException("Неправильна пошта або пароль.");
         }
     }
 
